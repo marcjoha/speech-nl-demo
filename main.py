91df6d2427459784dbd4f5abe132313f20c83ab9
Marcus Johansson <majohansson@gmail.com>
2016-09-20 17:50:53 +0100

    added cmd line support and bumped API versions

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import base64
 import httplib2
-from exceptions import Exception
-from flask import Flask, render_template, request, jsonify
-from oauth2client.client import GoogleCredentials
+from flask import Flask, jsonify, render_template, request
 from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
 
 app = Flask(__name__, template_folder=".")
 app.secret_key = "\xc3\xad\xb7\xaa\xc2\x82\xa4\xb0\n\xe2"
@@ -19,28 +18,39 @@ def get_service(service, version):
 def main():
   return render_template("main.html")
 
+@app.route("/", methods=['POST'])
+def upload():
+  file = request.files['file']
+  if file:
+    string = file.read()
+  response = language_api(string)
+  return jsonify(response)
+
 @app.route("/speech", methods=["POST"])
 def speech():
   audio = request.files["file"].read()
 
   payload = {
-    "initialRequest": {
+    "config": {
       "encoding": "LINEAR16",
       "sampleRate": 44100
     },
-    "audioRequest": {
+    "audio": {
       "content": base64.b64encode(audio).decode("UTF-8")
     },
   }
 
-  req = get_service("speech", "v1").speech().recognize(body=payload)
+  req = get_service("speech", "v1beta1").speech().syncrecognize(body=payload)
   response = req.execute()
   return jsonify(response)
 
 @app.route("/language", methods=["POST"])
 def language():
   string = request.form["string"]
+  response = language_api(string)
+  return jsonify(response)
 
+def language_api(string):
   payload = {
     "document": {
       "type": "PLAIN_TEXT",
@@ -54,7 +64,5 @@ def language():
     "encodingType": "UTF32"
   }
 
-  req = get_service("language", "v1alpha1").documents().annotateText(body=payload)
-  response = req.execute()
-  return jsonify(response)
-
+  req = get_service("language", "v1beta1").documents().annotateText(body=payload)
+  return req.execute()
